a039e39578d6e461392ba49229941b54ffa0a35e
Or Hay <ohay@chicagobooth.edu>
2018-11-07 22:50:44 +0000

    final

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206152419) do
+ActiveRecord::Schema.define(version: 20181107213115) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 20180206152419) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "characters", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "actor_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "directors", force: :cascade do |t|
     t.string "dob"
     t.string "name"
@@ -69,6 +77,7 @@ ActiveRecord::Schema.define(version: 20180206152419) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "director_id"
   end
 
 end
